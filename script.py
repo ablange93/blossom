d468a75619f4e7f392ec008887eb8b8e69783ca0
Andrew Lange <andrew.lange93@gmail.com>
2018-10-09 20:31:15 -0400

    Added hash function.

--- script.py
+++ script.py
@@ -1,5 +1,10 @@
+# HASH MAP #
 class HashMap:
   # ARRAY 
   def __init__(self, size):
     self.array_size = size
     self.array = [None for i in range(size)]
+    
+  # HASH FUNCTION | Sum character encodings of each character.
+  def hash(self, key):
+    hash_code = sum(key.encode())
