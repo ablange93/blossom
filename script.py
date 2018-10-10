533daeaa0fad758bf3f687e35b097cd1da602934
Andrew Lange <andrew.lange93@gmail.com>
2018-10-09 20:43:54 -0400

    Implemeneted seperate chaining by changing  construcutor to create array of linked lists.

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
+from linked_list import Node, LinkedList
+
 # HASH MAP #
 class HashMap:
   # ARRAY 
   def __init__(self, size):
     self.array_size = size
-    self.array = [None for i in range(size)]
+    self.array = [LinkedList() for i in range(size)]
     
   # HASH FUNCTION | Sum character encodings of each character.
   def hash(self, key):
